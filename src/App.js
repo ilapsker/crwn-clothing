30c55e4ebcd84469d42ab697c21fb092dabb8fb2
Ilya Lapsker <ilyalapsker@Ilyas-iMac.local>
2022-01-04 18:43:45 -0500

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 
 import HomePage from "./pages/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
